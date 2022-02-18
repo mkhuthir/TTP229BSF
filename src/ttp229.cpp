9ab06942f0d95701dedca8fc297d972b5857c3fd
Muthanna <mkhuthir@gmail.com>
2022-02-18 18:57:11 +0400

    first push

--- src/ttp229.cpp
+++ src/ttp229.cpp
@@ -1,1 +1,19 @@
+#include <Arduino.h>
+#include <ttp229.h>
 
+
+byte Read_TTP229_Keypad(void)
+{
+ byte Num;
+ byte Key_State = 0;
+ for(Num = 1; Num <= 16; Num++)
+ {
+  digitalWrite(SCL, LOW);
+  if (!digitalRead(SDO)){
+    Key_State = Num;
+  
+  }
+  digitalWrite(SCL, HIGH);
+ } 
+ return Key_State;
+}
--- src/ttp229.h
+++ src/ttp229.h
@@ -1,1 +1,13 @@
 
+#ifndef _ttp229_h_
+#define _ttp229_h_
+
+#define SCL 8
+#define SDO 9
+
+byte Read_TTP229_Keypad(void);
+
+
+
+
+#endif
