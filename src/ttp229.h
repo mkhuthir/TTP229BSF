32d04b1a1ea4f884aaff122bb60f52fcd72ea6d3
Muthanna <mkhuthir@gmail.com>
2022-02-19 12:31:34 +0400

    building class

--- src/ttp229.h
+++ src/ttp229.h
@@ -1,9 +1,21 @@
+/* 
+ *  Muthanna Alwahash 2020/21
+ *
+ */
 
 #ifndef _ttp229_h_
 #define _ttp229_h_
 
 #define SCL 8
 #define SDO 9
+
+
+class TTP229 {
+    public:
+        void TTP229(void);
+        void TTP229::getKeys(void);
+
+};
 
 byte Read_TTP229_Keypad(void);
 
